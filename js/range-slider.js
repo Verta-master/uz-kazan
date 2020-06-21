fd6f2ba160dd250514b9ab6aa4717bf2ee95191a
Verta-master <vertyagina@gmail.com>
2020-06-21 19:39:54 +0600

    Catalog

--- js/range-slider.js
+++ js/range-slider.js
@@ -2,8 +2,8 @@
 $( function() {
   $( "#slider-range" ).slider({
     range: true,
-    min: 500,
-    max: 10000,
+    min: 0,
+    max: 15000,
     values: [ 500, 10000 ],
     slide: function( event, ui ) {
       $( "#amount-min" ).val( ui.values[ 0 ] );
